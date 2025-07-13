b1c0750243546ba920f1e377ac00ca5b06b7812c
Euclides Guedes Catunda <euclides.catunda@gmail.com>
2025-07-13 01:11:29 -0300

    Add Flutter SDK configuration and update NDK version fallback in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.catundastudio.kanbatask"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = project.findProperty("flutter.ndkVersion") as String? ?: "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
