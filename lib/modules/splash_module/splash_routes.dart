829f56ff2a6b6d02b9a8e3ee208f99e98664c0a6
Euclides Catunda <euclides.catunda@gmail.com>
2025-07-15 22:54:19 -0300

    Add design system files: implement KanbaColors, KanbaText, and ThemeConfig; create SplashRoutes for navigation.

--- lib/modules/splash_module/splash_routes.dart
+++ lib/modules/splash_module/splash_routes.dart
@@ -1,7 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:kanbatask/modules/splash_module/presentation/splash/splash_page.dart';
 
-class SplashModule {
+class SplashRoutes {
   static GoRoute get route => GoRoute(
     path: '/splash',
     name: 'splash',
